1124c5fed9083c4179550c2d317ae514956ef61b
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-05-03 09:18:59 -0500

    Se crea el modelo Entrada para poder tener las entradas para el blog

--- blog_main/models.py
+++ blog_main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Entrada(models.Model):
+    creado_en = models.DateTimeField(auto_now_add=True)
+    modificado_en = models.DateTimeField(auto_now=True)
+    titulo = models.CharField(max_lenght=50, null=False, unique=True)
+    contenido = models.TextField(null=False)
+    slug = models.SlugField(max_length=50)
+
+    class Meta:
+        verbose_name = "Entrada"
+        verbose_name_plural = "Entradas"
+
+    def __str__(self):
+        return self.titulo
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.titulo)
+
+        super(Entrada, self).save(*args, **kwargs)
