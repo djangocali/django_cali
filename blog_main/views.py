5b1bfe2539ba24ab3c5929442efa2906d13a5292
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-05-06 11:21:45 -0500

    Definidas las vistas a construir, y una meta para llenar

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Crear todas las vistas usando ClassBased Views
+
+# Crear vista que lista las ultimas cinco entradas
+
+# Crear vista que muestre las entradas mas populares
+
+# Crear vista que invoca todas las entradas
+
+# Crear vista que lista todas las entradas de una categoria dada
+
+# Crear vista que muestre las entradas de una etiqueta dada
